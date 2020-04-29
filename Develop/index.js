2e7f4922dfbdf4db9093b880dff0b862adc19611
Johnny Li <reptile18@gmail.com>
2020-04-28 17:58:24 -0700

    add new feature for changing dates

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
   var schedule;
   var activeDataHour;
   
-  $("#currentDay").text(dateString);
-  function fakeStorage() {
-    schedule = {};
-    schedule[dateString] = {};
-    schedule[dateString]["t9"] = "Meet with stakeholders";
-    schedule[dateString]["t13"] = "Lunch";
+  function updateDate() {
+    dateString = currentDate.format("MMMM Do YYYY")
+    $("#currentDay").text(dateString);
   }
+  updateDate();
+
   function load() {
     schedule = localStorage.getItem("work-day-schedule");
     if (schedule == null) {
@@ -21,16 +20,21 @@ $(document).ready(function() {
       schedule = JSON.parse(schedule);
     }
   }
+  load();
+
   function save() {
     let scheduleString = JSON.stringify(schedule);
     localStorage.setItem("work-day-schedule",scheduleString);
   }
-  load();
+  
   function update() {
     for (var t = 9; t < 18; t++)
     {
-      let item = schedule[dateString]["t" + t];
-      item = item==null?"":item;
+      let item;
+      if (typeof schedule[dateString] !== "undefined") {
+        item = schedule[dateString]["t" + t];
+        item = item==null?"":item;
+      }
       addRow(t,item);
     }
   }
@@ -43,7 +47,8 @@ $(document).ready(function() {
     var saveButtonDiv = $("<div>").addClass("saveBtn col-sm-1 d-flex justify-content-center align-items-center").append(saveButton).attr("data-hour",time);
 
     // highlighting
-    if (time < currentDate.hour()) {
+    // old behavior
+    /*if (time < currentDate.hour()) {
       timeBlock.addClass("past");
     }
     else if (time == currentDate.hour()) {
@@ -51,6 +56,24 @@ $(document).ready(function() {
     }
     else {
       timeBlock.addClass("future");
+    }*/
+    // bonus behavior
+    if (currentDate.dayOfYear() < moment().dayOfYear()) {
+      timeBlock.addClass("past");
+    }
+    else if (currentDate.dayOfYear() > moment().dayOfYear()) {
+      timeBlock.addClass("future");
+    }
+    else {
+      if (time < currentDate.hour()) {
+        timeBlock.addClass("past");
+      }
+      else if (time == currentDate.hour()) {
+        timeBlock.addClass("present");
+      }
+      else {
+        timeBlock.addClass("future");
+      }
     }
 
     divRow.append(hour).append(timeBlock).append(saveButtonDiv);
@@ -59,16 +82,14 @@ $(document).ready(function() {
   update();
 
   function onOutsideTextAreaClick(event) {
-    alert(1);
     if (!$("#edit"+activeDataHour).is(event.target) && $("#edit"+activeDataHour).has(event.target).length === 0) {
       $("#item"+activeDataHour).addClass("d-flex").removeClass("d-none");
       $("#edit"+activeDataHour).remove();
       $(document).off("click");
     }
   }
-
-  // Events
-  $(".time-block").click(function(event) {
+  function onTimeBlockClick(event) {
+    console.log("time_click");
     event.stopPropagation();
     activeDataHour = $(this).attr("data-hour");
     var itemInput = $("<textarea>").addClass("time-block col-sm-10 d-flex justify-content-center align-items-center").attr("id","edit"+activeDataHour).val($(this).text());
@@ -78,9 +99,8 @@ $(document).ready(function() {
     $(this).before(itemInput);
     $(this).addClass("d-none").removeClass("d-flex");
     itemInput.focus();
-  });
-
-  $(".saveBtn").click(function() {
+  }
+  function onSaveBtnClick() {
     let datahour = $(this).attr("data-hour");
     $("#item"+datahour).addClass("d-flex").removeClass("d-none").text($("#edit"+datahour).val());
     $(document).off("click");
@@ -88,7 +108,44 @@ $(document).ready(function() {
     schedule[dateString]["t"+datahour] = $("#item"+datahour).text();
     save();
     $("#edit"+datahour).remove();
+  }
+
+  function offEvents() {
+    $(".time-block").off("click","**");
+    $(".saveBtn").off("click","**");
+  }
+
+  function onEvents() {
+    $(".time-block").click(onTimeBlockClick);
+    $(".saveBtn").click(onSaveBtnClick);
+  }
+
+  function newDay() {
+    offEvents();
+    $(".container").empty();
+    updateDate();
+    update();
+    onEvents();
+    if (typeof schedule[dateString] === "undefined") {
+      schedule[dateString] = {};
+    }
+  }
+
+  // Events
+  onEvents();
+
+  // Bonus
+  
+  $("#prevDayIcon").click(function(event) {
+    currentDate.subtract(1,"d");
+    newDay();
   });
+
+  $("#nextDayIcon").click(function(event) {
+    currentDate.add(1,"d");
+    newDay();
+  }); 
+  
 
 
 });
